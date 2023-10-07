c2f6cb4700b94c33a88abfaa01baa90d1a85ba56
Suwan_Sankaja <sankajahls.20@uom.lk>
2023-10-07 12:43:04 +0530

    ss

--- stone/src/markings.pas
+++
@@ -1,5 +0,0 @@
-program markings; 
-begin
-   writeln('Delectus voluptas autem. Dolores provident dolor esse a vel quisquam. A facilis qui voluptatum fugit libero similique.');
-   writeln('253');
-end.
